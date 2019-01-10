4da87801453d6384de09cdffbeb5bd1cfc863a51
Victor Algarin <victor@syncbak.corp>
2019-01-09 21:37:29 -0600

    Added VechicleReservationsController; DB work in progress

--- TruVoyageDB.sql
+++ TruVoyageDB.sql
@@ -198,21 +198,52 @@ AS
 	END
 GO
 
-print '' print '*** Creating VehicleReservation Table ***'
+print '' print '*** Creating Location Table ***'
 GO
-/* ***Object: Table[dbo].[VehicleReservation]*** */
+/* ***Object: Table[dbo].[Location]*** */
 
-CREATE TABLE [dbo].[VehicleReservation](
-	[ReservationID]		[int]IDENTITY(100000,1)	NOT NULL,	
-	[RatePerDay]		[decimal](7,2)			NOT NULL,
-	[ReservationStart]	[date]					NOT NULL,
-	[ReservationEnd]	[date]					NOT NULL,
-	[CustomerID]		[int]					NOT NULL,
-	[VIN]				[int]					NOT NULL,
-	[TotalRegisteredClients] [int]				NOT NULL,
+CREATE TABLE [dbo].[Location](
+	[LocationID]		[int]IDENTITY(100000,1)	NOT NULL,	
+	[Address]			[varchar](300)			NOT NULL,
+	[City]				[varchar](300)			NOT NULL,
+	[State]				[varchar](50)			NOT NULL,
+	[Zip]				[int]					NOT NULL,
+	[Country]			[varchar](200)			NOT NULL DEFAULT "United States",
 	
-	CONSTRAINT [pk_ReservationID] PRIMARY KEY ([ReservationID] ASC)
+	CONSTRAINT [pk_LocationID] PRIMARY KEY ([LocationID] ASC)
 )
+GO
+
+print '' print '*** Inserting Location Sample Records ***'
+GO
+INSERT INTO [dbo].[Location]
+		([Address], [City], [State], [Zip])
+	VALUES
+		("1665 Union St", "Brooklyn", "NY", 11213),
+		("3638 College Dr", "Oceanside", "CA", 92056),
+		("606 Glynn Springs Dr", "Williamsburg", "VA", 23188)
+GO
+
+print '' print '*** Creating RentalCompany Table ***'
+GO
+/* ***Object: Table[dbo].[RentalCompany]*** */
+
+CREATE TABLE [dbo].[RentalCompany](
+	[CompanyID]			[int]IDENTITY(100000,1)	NOT NULL,	
+	[CompanyName]		[varchar](100)			NOT NULL,
+	[LocationID]		[varchar](300)			NOT NULL
+	
+	CONSTRAINT [pk_CompanyID] PRIMARY KEY ([CompanyID] ASC)
+)
+GO
+print '' print '*** Inserting RentalCompany Sample Records ***'
+GO
+INSERT INTO [dbo].[RentalCompany]
+		([CompanyName], [LocationID])
+	VALUES
+		("Wertz", 100000),
+		("Trivago Now", 100001),
+		("Jarvis", 100002)
 GO
 
 print '' print '*** Creating Customer Table ***'
@@ -228,23 +259,100 @@ CREATE TABLE [dbo].[Customer](
 	[City]				[varchar](50)			NOT NULL,
 	[State]				[varchar](2)			NOT NULL,
 	[Zip]				[int]					NOT NULL,
-	[Phone]				[int]					NOT NULL,
+	[Phone]				[varchar](14)			NOT NULL,
 	[Email]				[varchar](300)			NOT NULL,
 	[PartySize]			[int]					NOT NULL,
-	[DriversLicense]	[varchar](30)			NOT NULL
+	[DriversLicense]	[varchar](30)			DEFAULT "N/A"
 	
 	CONSTRAINT [pk_CustomerID] PRIMARY KEY ([CustomerID] ASC)
 )
 GO
 
-/* print '' print '**** Creating Movies GenreID Foreign Key ****'
+print '' print '*** Inserting Customer Sample Records ***'
 GO
-ALTER TABLE [dbo].[Movies] WITH NOCHECK
-	ADD CONSTRAINT [fk_GenreID] FOREIGN KEY ([GenreID])
-	REFERENCES [dbo].[Genre] ([GenreID])
+INSERT INTO [dbo].[Customer]
+		([FirstName], [LastName], [DOB], [StreetAddress], [City], [State], [Zip], [Phone], [Email], [PartySize], [DriversLicense])
+	VALUES
+		("Jimmy", "Faslane", "05/17/1975", "404 FU Lane", "Eat My", "AZ", 55545, 3191234567, "no@no.com", 2, "123CC4567"),
+		("Wyatt", "Eauyp", "07/11/1991", "503 Error Dr", "FixYo Shiz", "NY", 11213, 3655785555, "yes@yes.com", 4, "894AS555")
+GO
+
+print '' print '*** Creating VehicleReservation Table ***'
+GO
+/* ***Object: Table[dbo].[VehicleReservation]*** */
+
+CREATE TABLE [dbo].[VehicleReservation](
+	[ReservationID]		[int]IDENTITY(100000,1)	NOT NULL,	
+	[CustomerID]		[int]					NOT NULL,		
+	[CompanyID]			[int]					NOT NULL,
+	[VIN]				[varchar](300)			NOT NULL,
+	[RatePerDay]		[decimal](7,2)			NOT NULL,	
+	[ReservationStart]	[date]					NOT NULL,
+	[ReservationEnd]	[date]					NOT NULL,
+	[PickUpTime]		[time]					NOT NULL,
+	[DropOffTime]		[time]					NOT NULL,	
+	[PickUpLocation]	[varchar](300)			NOT NULL,
+	[DropOffLocation]	[varchar](300)			NOT NULL
+	
+	CONSTRAINT [pk_ReservationID] PRIMARY KEY ([ReservationID] ASC)
+)
+GO
+
+/* print '' print '*** Inserting VehicleReservation Sample Records ***'
+GO
+INSERT INTO [dbo].[VehicleReservation]
+		([CustomerID], [CompanyID], [VIN], [RatePerDay], [ReservationStart], [ReservationEnd], [PickUpTime], [DropOffTime], [PickUpLocation], [DropOffLocation])
+	VALUES
+		(100000, 100000, "5FSH67562CGRFH345", 46.50, "01/09/2019","01/10/2019", "13:30:00", "19:00:00",,100000)
+GO */
+
+-- SELECT [RentalCompany].[LocationID] FROM [RentalCompany] INNER JOIN [Location] ON [RentalCompany].[LocationID] = [Location].[LocationID],
+
+
+-- PROBABLY NEED TO MAKE A LOOK UP TABLE, OR JUST DO THIS IN A STORED PROCEDURE
+print '' print '*** Inserting VehicleReservation Sample Records ***'
+GO
+INSERT INTO [dbo].[VehicleReservation]
+		([CustomerID], [CompanyID], [VIN], [RatePerDay], [ReservationStart], [ReservationEnd], [PickUpTime], [DropOffTime], [PickUpLocation], [DropOffLocation]) 
+	VALUES
+		(100000, 100000, "5FSH67562CGRFH345", 46.50, "01/09/2019","01/10/2019", "13:30:00", "19:00:00")
+INSERT INTO [dbo].[VehicleReservation]
+	([PickUpLocation], [DropOffLocation])
+	SELECT RentalCompany.LocationID, RentalCompany.LocationID
+	FROM RentalCompany
+	INNER JOIN VehicleReservation ON VehicleReservation.CompanyID = RentalCompany.CompanyID
+GO
+
+/* print '' print '*** Inserting VehicleReservation Location Sample Records ***'
+GO
+INSERT INTO [dbo].[VehicleReservation]([PickUpLocation], [DropOffLocation])
+	SELECT [LoctionID], [LocationID]
+	FROM [dbo].[RentalCompany]
+	WHERE [RentalCompany].[CompanyID] = [VehicleReservation].[CompanyID]
+GO */
+
+
+print '' print '*** Creating VehicleReservation CustomerID foreign key ***'
+GO
+ALTER TABLE [dbo].[VehicleReservation]  WITH NOCHECK 
+	ADD CONSTRAINT [FK_CustomerID] FOREIGN KEY([CustomerID])
+	REFERENCES [dbo].[Customer] ([CustomerID])
 	ON UPDATE CASCADE
+GO 
+
+print '' print '*** Creating VehicleReservation CompanyID foreign key ***'
 GO
- */
+ALTER TABLE [dbo].[VehicleReservation]  WITH NOCHECK 
+	ADD CONSTRAINT [FK_CompanyID] FOREIGN KEY([CompanyID])
+	REFERENCES [dbo].[RentalCompany] ([CompanyID])
+	ON UPDATE CASCADE
+GO 
 
-
+print '' print '*** Creating VehicleReservation VIN foreign key ***'
+GO
+ALTER TABLE [dbo].[VehicleReservation]  WITH NOCHECK 
+	ADD CONSTRAINT [FK_VIN] FOREIGN KEY([VIN])
+	REFERENCES [dbo].[Vehicle] ([VIN])
+	ON UPDATE CASCADE
+GO 
 
